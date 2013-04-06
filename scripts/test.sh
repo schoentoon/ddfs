6891cb7c28c8f378803a913c1e2676ee8d75aa44
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-04-06 17:25:14 +0200

    Let's give the server a bit of time to actually start

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,7 @@ cd $TMP
 mkdir input output
 ../bin/ddfs_server -f input &
 SERVER=$?
+sleep 3 #Give the server some time to start..
 ../bin/ddfs -s 127.0.0.1 -f output &
 CLIENT=$?
 dd if=/dev/urandom of=input/testfile bs=1 count=1024
