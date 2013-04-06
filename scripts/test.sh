06e97c50153802b51d2ee341b340f2116128fb36
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-04-06 10:09:10 +0200

    Added a simple test script
    This will simply start the server and the client, write a random file
    to the input folder. It'll show a md5sum of both the input and the output
    file. Currently it'll only create a file of 1024 bytes. Might modify it
    later on so it'll create a random amount of files with random file sizes.

---
+++ scripts/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+TMP=`mktemp -d -p .`
+SRC=`pwd`
+cd $TMP
+mkdir input output
+../bin/ddfs_server -f input &
+SERVER=$?
+../bin/ddfs -s 127.0.0.1 -f output &
+CLIENT=$?
+dd if=/dev/urandom of=input/testfile bs=1 count=1024
+md5sum input/testfile
+sleep 1 #Just to be sure..
+md5sum output/input/testfile
+cd $SRC
+rm -rfv $TMP
+kill $CLIENT
+kill $SERVER
